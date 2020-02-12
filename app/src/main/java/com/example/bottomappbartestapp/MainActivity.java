df6316240c1c9bbdde3376c0c6acb23b88e6f644
JereIsThere <46347376+JereIsThere@users.noreply.github.com>
2020-02-13 00:16:56 +0100

    initial commit

---
+++ app/src/main/java/com/example/bottomappbartestapp/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.bottomappbartestapp;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    MenuItem next;
+    MenuItem help;
+    Menu bab1Menu;
+    FloatingActionButton fab1;
+    BottomAppBar bab1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fab1 = findViewById(R.id.fab1);
+        bab1 = findViewById(R.id.bab1);
+
+        fab1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyLogClass.toast("clicked fab", getBaseContext());
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.bab_1_menu, menu);
+
+        MyLogClass.d(menu.getItem(0).getItemId());
+        MyLogClass.d(R.id.menu_bab1_help);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menu_bab1_help:
+                MyLogClass.toast("help", getBaseContext());
+                return true;
+            case R.id.menu_bab1_next:
+                MyLogClass.toast("next", getBaseContext());
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/bottomappbartestapp/MyLogClass.java
@@ -0,0 +1,23 @@
+package com.example.bottomappbartestapp;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * a class to call {@link Log#d} with the predefined tag "myCustomTag"
+ */
+class MyLogClass {
+
+    public static void d(String message) {
+        Log.d("myCustomTag", message);
+    }
+
+    public static void d(Object message) {
+        Log.d("myCustomTag", message.toString());
+    }
+
+    public static void toast(String str, Context context){
+        Toast.makeText(context, str, Toast.LENGTH_LONG).show();
+    }
+}
